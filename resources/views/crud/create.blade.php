3b6235e9900ae5bad1fc485e2096d49a98412ccf
Barretttttt <n01173084@ospreys.unf.edu>
2017-08-08 11:02:07 -0400

    Create Form
    renamed title->volunteer name, post -> opportunity matches

--- resources/views/crud/create.blade.php
+++ resources/views/crud/create.blade.php
@@ -6,13 +6,13 @@
   <form method="post" action="{{url('crud')}}">
     <div class="form-group row">
       {{csrf_field()}}
-      <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Title</label>
+      <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Volunteer Name</label>
       <div class="col-sm-10">
         <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="title">
       </div>
     </div>
     <div class="form-group row">
-      <label for="smFormGroupInput" class="col-sm-2 col-form-label col-form-label-sm">Post</label>
+      <label for="smFormGroupInput" class="col-sm-2 col-form-label col-form-label-sm">Opportunity Matches</label>
       <div class="col-sm-10">
         <textarea name="post" rows="8" cols="80"></textarea>
       </div>
