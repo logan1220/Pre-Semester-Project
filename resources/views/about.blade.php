41f244ddfbd2234e78e526c9be6f83686dee06c9
madison-gourde15 <30449490+madison-gourde15@users.noreply.github.com>
2017-08-09 19:50:31 -0400

    About changes
    changed about us stuff

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,22 @@
                     <div class="panel-heading">About Us</div>
 
                     <div class="panel-body">
-                        About Us
+                        <div style="text-align:left;"><p><div style="text-decoration:underline"><b>Madison Gourde</b></div><p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Madison was born in North Dakota, but was raised from the age of 5 and up in various areas of Florida before settling in Jacksonville. She started at UNF in 2014 for her freshman year in hopes of getting a degree in Accounting before making a hard, but necessary, decision and switching majors. She expects to graduate in May of 2018 with a Bachelor’s in Information Science.
+                        </div><p><p>
+                        <hr width="80%">
+                        <div style="text-align:right;"><p><div style="text-decoration:underline"><b>Reggie Jackson</b></div><p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reggie was born and raised in Jacksonville, Florida and he is hoping to graduate from the University of North Florida with a degree in the Information Systems program. He also expects to graduate in the Spring of 2018, and is currently employed at <i>4 Rivers Smokehouse</i> as a carver. He’s been waiting his whole life to graduate and get a good job so he can show his friends who have given up in school that they can do it. His next term he plans to earn an internship and prove to employers that he belongs in his field.
+                        </div><p>
+                        <hr width="80%">
+                        <div style="text-align:left;"><p><div style="text-decoration:underline"><b>Logan Sirdevan</b></div><p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Logan was born in Atlanta, Georgia but moved to Jacksonville, Florida shortly thereafter. He transferred to the University of North Florida from Florida State University in 2016 and expects to graduate with a Bachelor’s degree in Computer Information Sciences with a minor in Business Administration before the Summer of 2018. Logan is employed by a company called Marketing Armor LLC, where he is the lead Junior Software Developer in charge of back end development for several clients. After obtaining his degree, Logan plans to further his career in software development using web based languages.
+                        </div><p>
+                        <hr width="80%">
+                        <div style="text-align:right;"><p><div style="text-decoration:underline"><b>Wesley Tucker</b></div><p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Wesley was born and raised in Jacksonville, Florida. As a transfer student to the University of North Florida from St. Johns River State College in 2016, he expects to graduate with a Bachelor’s degree in Information Science at the end of the Spring 2018 semester. Wesley is currently employed at The First Tee of North Florida where he is a head coach and has responsibilities including mentoring children ages 5 to 17 and providing a safe environment for them to learn and have fun.
+                        </div><p>
+
                     </div>
 
                     <img src="http://i.imgur.com/rz4q2zp.jpg"/>
