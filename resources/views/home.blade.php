357eedceda37af3f7c5f6849eb831bbab567fe93
Logan <logan@sirdevan.com>
2017-08-22 12:22:55 -0400

    Change home screen
    Made it a little prettier

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,21 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Welcome admin <b>{{ Auth::user()->name }}</b></div>
+                    <div class="row">
+                        <div class="panel-body">
+                            <p style="padding-left:15px;">Please choose a following action:</p>
 
-                <div class="panel-body">
-                    You are logged in!
+                            <div class="col-md-1">
+                                <a href="{{ route('volunteer.index') }}"><button class="btn btn-success">Manage Volunteers</button></a>
+                            </div>
+
+                            <div class="col-md-offset-3">
+                                <a href="{{ route('opportunity.index') }}"><button class="btn btn-success">Manage Opportunities</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                @foreach($users as $user)
-                <ul>
-                    <li>User {{ $user->id }}: {{ $user->name }}</li>
-                </ul>
-                @endforeach
             </div>
         </div>
     </div>
